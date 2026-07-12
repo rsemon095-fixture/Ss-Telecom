48a94fc9c0a90fc0dc3a41cd8c3cd42bbbcbc7f7
rsemon095-fixture <rsemon095@gmail.com>
2026-07-12 14:36:25 +0600

    Implement chat feature using Firebase Realtime Database
    Add chat functionality with Firebase integration for real-time messaging.

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,55 @@
+import { rtdb } from "../firebase.js";
 
+import {
+ref,
+push,
+onValue
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+const chatRef = ref(rtdb,"chat");
+
+const userName=document.getElementById("userName");
+const message=document.getElementById("message");
+const sendBtn=document.getElementById("sendBtn");
+const chatBox=document.getElementById("chatBox");
+
+sendBtn.addEventListener("click",()=>{
+
+const name=userName.value.trim();
+const text=message.value.trim();
+
+if(!name || !text){
+alert("Name এবং Message লিখুন");
+return;
+}
+
+push(chatRef,{
+name,
+text,
+time:Date.now()
+});
+
+message.value="";
+
+});
+
+onValue(chatRef,(snapshot)=>{
+
+chatBox.innerHTML="";
+
+snapshot.forEach((item)=>{
+
+const data=item.val();
+
+chatBox.innerHTML+=`
+<div class="message">
+<strong>${data.name}</strong><br>
+${data.text}
+</div>
+`;
+
+});
+
+chatBox.scrollTop=chatBox.scrollHeight;
+
+});
